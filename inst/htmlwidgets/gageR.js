9038296235fea69a0b3c523efa0aab08e334b334
iBrianCarter <bcarter_ie@yahoo.co.uk>
2015-09-30 20:04:53 +0100

    First running gage2

--- inst/htmlwidgets/gageR.js
+++ inst/htmlwidgets/gageR.js
@@ -5,20 +5,35 @@ HTMLWidgets.widget({
   type: 'output',
 
   initialize: function(el, width, height) {
-
-    return {
+    el.style.height = height + "px";
+		el.style.width = width + "px";
+    return el;
+    //{
       // TODO: add instance fields as required
-    }
+    //}
 
   },
 
   renderValue: function(el, x, instance) {
 
-    el.innerText = x.message;
+    //el.innerText = x.message;
+    var gauge1 = new JustGage({
+			parentNode: el,
+			title: x.title,
+			value: x.value,
+			min: x.min,
+			max: x.max,
+			decimals: 0,
+			counter: true
+		});
+
 
   },
 
   resize: function(el, width, height, instance) {
+  	el.style.height = height + "px";
+		el.style.width = width + "px";
+		return el;
 
   }
 
